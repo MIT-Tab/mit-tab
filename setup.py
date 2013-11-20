4ba91f43070ddd8736e0aef23542760454fed2a3
Joseph Lynch <jolynch@mit.edu>
2013-11-20 09:51:30 -0800

    Making mit-tab a proper python package

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='mittab',
+    version='0.1.0',
+    description='A web application to tabulate and run APDA tournaments',
+    author='Joseph Lynch',
+    author_email='jolynch@mit.edu',
+    url='https://github.com/jolynch/mit-tab.git',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    setup_requires=['setuptools'],
+    install_requires=[
+        'Django=1.4.5',
+        'South',
+        'mock'
+        'pytest'
+    ],
+    license='MIT License'
+)
