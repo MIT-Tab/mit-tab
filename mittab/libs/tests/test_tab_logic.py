48afb03fe94c45e24d0c7025410aff67792a9b01
Joseph Lynch <jolynch@mit.edu>
2014-03-07 20:24:20 -0800

    Adding some pairing testing methods

--- mittab/libs/tests/test_tab_logic.py
+++ mittab/libs/tests/test_tab_logic.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from mittab.apps.tab.models import Debater
 from mittab.apps.tab.models import Team
+from mittab.apps.tab.models import TabSettings
+from mittab.apps.tab.models import Round
+import mittab.libs.assign_judges as assign_judges
 import mittab.libs.tab_logic as tab_logic
+from mittab.libs.tests.assertion import assert_nearly_equal
 from mittab.libs.tests.data.load_data import load_debater_rankings
 from mittab.libs.tests.data.load_data import load_team_rankings
-from mittab.libs.tests.assertion import assert_nearly_equal
+from mittab.libs.tests.helpers import generate_results
 
 
 class TestRankingLogic(TestCase):
@@ -45,7 +49,34 @@ class TestPairingLogic(TestCase):
     """Tests that the the generate pairings are correct"""
     fixtures = ['testing_db']
 
-    def test_pair_round_one(self):
-        """ Tests that the first pairing results in a reasonable result """
+    def pair_round(self):
         tab_logic.pair_round()
+        current_round = TabSettings.objects.get(key='cur_round')
+        current_round.value = current_round.value + 1
+        current_round.save()
+
+    def assign_judges(self):
+        cur_round = self.round_number()
+        panel_points = []
+        rounds = list(Round.objects.filter(round_number=cur_round))
+        judges = [ci.judge for ci in
+                  CheckIn.objects.filter(round_number=cur_round)]
+        assign_judges.add_judges(rounds, judges, panel_points)
+
+    def round_number(self):
+        return TabSettings.objects.get(key='cur_round').value - 1
+
+    def test_pairing_tournament(self):
+        """
+        Tests that we can pair round 1 through round 5, checking for various
+        things along the way. This is all one big method because the goal is to
+        test the entire pipeline in one go, and make sure a full tournament
+        works. Also, test fixture loading is painfully slow
+        """
+        assert self.round_number() == 0
+        self.pair_round()
+        assert self.round_number() == 1
+        self.assign_judges()
+        generate_results(self.round_number(), 0.05, 0.05)
+        assert tab_logic.ready_to_pair(self.round_number() + 1)
 
