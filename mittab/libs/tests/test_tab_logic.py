f5a19bd3a40c991339c84b48e1bfa68188623305
Joseph Lynch <jolynch@mit.edu>
2014-03-09 11:06:50 -0700

    Test pairing all 5 rounds

--- mittab/libs/tests/test_tab_logic.py
+++ mittab/libs/tests/test_tab_logic.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from mittab.apps.tab.models import CheckIn
 from mittab.apps.tab.models import Debater
+from mittab.apps.tab.models import Judge
 from mittab.apps.tab.models import Team
 from mittab.apps.tab.models import TabSettings
 from mittab.apps.tab.models import Round
@@ -9,6 +11,8 @@ from mittab.libs.tests.assertion import assert_nearly_equal
 from mittab.libs.tests.data.load_data import load_debater_rankings
 from mittab.libs.tests.data.load_data import load_team_rankings
 from mittab.libs.tests.helpers import generate_results
+
+import random
 
 
 class TestRankingLogic(TestCase):
@@ -55,10 +59,30 @@ class TestPairingLogic(TestCase):
         current_round.value = current_round.value + 1
         current_round.save()
 
+    def generate_checkins(self):
+        cur_round = self.round_number()
+        round_count = Round.objects.filter(round_number=cur_round).count()
+        desired_judges = int(round(round_count * 1.2))
+        checkin_count = CheckIn.objects.filter(round_number=cur_round).count()
+
+        available = Judge.objects.exclude(judges__round_number=cur_round)
+        available = available.filter(checkin__round_number=cur_round)
+        available = list(available)
+        random.shuffle(available)
+        if checkin_count < desired_judges:
+            num_to_checkin = desired_judges - checkin_count
+            judges_to_checkin = available[:num_to_checkin] 
+            checkins = [CheckIn(judge=judge, round_number=cur_round) for
+                        judge in judges_to_checkin]
+            for checkin in checkins:
+                checkin.save()
+
+
     def assign_judges(self):
         cur_round = self.round_number()
         panel_points = []
         rounds = list(Round.objects.filter(round_number=cur_round))
+        self.generate_checkins()
         judges = [ci.judge for ci in
                   CheckIn.objects.filter(round_number=cur_round)]
         assign_judges.add_judges(rounds, judges, panel_points)
@@ -66,17 +90,26 @@ class TestPairingLogic(TestCase):
     def round_number(self):
         return TabSettings.objects.get(key='cur_round').value - 1
 
+    def check_pairing(self, round_number, last):
+        assert self.round_number() == round_number
+        self.pair_round()
+        assert self.round_number() == round_number + 1
+        self.assign_judges()
+        generate_results(round_number + 1, 0.05, 0.05)
+        if round_number + 2 != last:
+            assert tab_logic.ready_to_pair(round_number + 2)
+
     def test_pairing_tournament(self):
         """
         Tests that we can pair round 1 through round 5, checking for various
         things along the way. This is all one big method because the goal is to
         test the entire pipeline in one go, and make sure a full tournament
-        works. Also, test fixture loading is painfully slow
+        works. Also, test fixture loading is painfully slow and we really only
+        need it to set the initial stte of the tournament with real world
+        data.
         """
-        assert self.round_number() == 0
-        self.pair_round()
-        assert self.round_number() == 1
-        self.assign_judges()
-        generate_results(self.round_number(), 0.05, 0.05)
-        assert tab_logic.ready_to_pair(self.round_number() + 1)
+        last_round = 6
+        for _ in range(1, last_round):
+            self.check_pairing(self.round_number(), last_round)
+
 
