49de40affb1b699a5dc3772c9303fc28e9c1df29
Ben Muschol <muschol.b@husky.neu.edu>
2016-06-19 14:25:43 -0400

    disallow e-ballots by default

--- mittab/apps/tab/middleware.py
+++ mittab/apps/tab/middleware.py
@@ -5,17 +5,24 @@ from django.http import HttpResponseRedirect
 from django.core.cache import cache
 
 login_white_list = ('/accounts/login/', '/static/css/stylesheet.css',
-        '/static/images/title_banner.png', '/pairings/pairinglist/',
-        '/e_ballots/', '/stat')
+        '/static/images/title_banner.png', '/pairings/pairinglist/', '/stat')
+
 e_ballot_regex = re.compile("/e_ballots/\S+")
 #backend_white_list = ('/static/css/stylesheet.css', '/enter_tab/', '/static/images/title_banner.png')
 
 class Login:
     "This middleware requires a login for every view"
     def process_request(self, request):
-        whitelisted = (request.path in login_white_list) or e_ballot_regex.match(request.path)
+        whitelisted = (request.path in login_white_list)
+        whitelisted = whitelisted or self._is_eballot_url(request.path)
         if not whitelisted and request.user.is_anonymous():
             if request.POST:
                 return login(request)
             else:
                 return HttpResponseRedirect('/accounts/login/?next=%s' % request.path)
+
+    def _is_eballot_url(self, path):
+        if not TabSettings.get('allow_eballots', 0) == 1:
+            return False
+        return (path == '/e_ballots/') or e_ballot_regex.match(request.path)
+
