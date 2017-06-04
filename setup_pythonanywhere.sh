badfef6a3e8c3184e258b199183f65838be504ae
Ben Muschol <benmuschol@gmail.com>
2017-06-04 13:57:28 -0400

    one last thing

--- setup_pythonanywhere.sh
+++ setup_pythonanywhere.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # sets up a server on PythonAnywhere
-# ONLY WORKS IF YOU ARE NOT USING CUSTOM DOMAINS
 
 echo $1
 if [ -z $1 ]; then
@@ -35,6 +34,6 @@ python manage.py collectstatic
 # https://gist.github.com/BenMusch/f3e950298001b2717882a39fc5ca3074
 # If you wish to change the WSGI, change the Gist then update the link
 echo -e "${BLUE}STEP 4: Configuring server${NC}"
-curl -o /var/www/$(echo $USER)_pythonanywhere_com_wsgi.py https://gist.githubusercontent.com/BenMusch/f3e950298001b2717882a39fc5ca3074/raw/f97ba6cb20946b7cf21916be3f90b43d333874ca/gistfile1.py
+curl -o $WSGI https://gist.githubusercontent.com/BenMusch/f3e950298001b2717882a39fc5ca3074/raw/f97ba6cb20946b7cf21916be3f90b43d333874ca/gistfile1.py
 echo -e "${GREEN}Success! Read this document to see how to finish the process:${NC}"
 echo -e "${BLUE}https://docs.google.com/document/d/1j4r8UwtJqeQBz2rhNAZo1ITt40DaDe0cz6G2iiTz414/edit${NC}"
