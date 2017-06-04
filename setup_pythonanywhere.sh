40266b37e3f6523e0f32295e2bbb988ac3c23604
Ben Muschol <benmuschol@gmail.com>
2017-06-04 12:58:02 -0400

    add command to setup wsgi

--- setup_pythonanywhere.sh
+++ setup_pythonanywhere.sh
@@ -7,3 +7,5 @@ workon mittab
 pip install -r requirements.txt
 python manage.py changepassword tab
 python manage.py collectstatic
+
+curl -o /var/www/$(echo $USER)_pythonanywhere_com_wsgi.py https://gist.githubusercontent.com/BenMusch/f3e950298001b2717882a39fc5ca3074/raw/f3a618ca1118771d4529ae55ae8dcee9676363a3/gistfile1.py
