33b545b9e9f077a286ae05b4a6aa6c3229c314b9
Ben Muschol <benmuschol@gmail.com>
2017-06-20 23:10:09 -0400

    convert judges

--- mittab/libs/data_import/base.py
+++ mittab/libs/data_import/base.py
@@ -2,6 +2,10 @@ from decimal import *
 import xlrd
 from xlwt import Workbook
 
+from mittab.apps.tab.models import *
+from mittab.apps.tab.forms import *
+
+
 class DataImporter(object):
     num_columns = 1
 
@@ -14,6 +18,7 @@ class DataImporter(object):
         except:
             self.errors.append('ERROR: Insufficient columns in sheet. No data read.')
 
+    # TODO: Avoid repeated calcuations
     def num_rows(self):
         num_rows = 0
         found_end = False
@@ -27,7 +32,7 @@ class DataImporter(object):
 
         return num_rows
 
-    def read_rows(self):
+    def save(self):
         for i in range(1, self.num_rows()):
             self.read_row(i)
 
@@ -36,6 +41,77 @@ class DataImporter(object):
 
     def _value_at(row, column):
         return self.worksheet.cell(row, column).value
+
+
+class JudgeImporter(DataImporter):
+    num_columns = 2
+
+    def read_row(self, row_num):
+        #Load and validate Judge's Name
+        judge_name = self._value_at(row_num, 0)
+
+        try:
+            Judge.objects.get(name=judge_name)
+            self.errors.append(judge_name + ": Duplicate Judge Name")
+        except:
+            return
+
+        #Load and validate judge_rank
+        judge_rank = self._value_at(row_num, 1)
+        try:
+            judge_rank = round(float(judge_rank), 2)
+        except:
+            self.errors.append(judge_name + ": Rank not number")
+
+        if judge_rank > 100 or judge_rank < 0:
+            self.errors.append(judge_name + ": Rank should be between 0-100")
+
+        #Because this data is not required, be prepared for IndexErrors
+        try:
+            judge_phone = str(int(self._value_at(row_num, 2)))
+        except IndexError:
+            judge_phone = ''
+        try: 
+            judge_provider = self._value_at(row_num, 3)
+        except IndexError:
+            judge_provider = ''
+
+        #iterate through schools until none are left
+        col_num = 4
+        schools = []
+        while(True):
+            try:
+                judge_school = sh._value_at(row_num, col_num)
+                #If other judges have more schools but this judge doesn't, we get an empty string
+                #If blank, keep iterating in case user has a random blank column for some reason
+                if judge_school != '':
+                    try:
+                        #Get id from the name because JudgeForm requires we use id
+                        s = School.objects.get(name__iexact=judge_school).id 
+                        schools.append(s)
+                    except IndexError:
+                        break
+                    except:
+                        try:
+                            s = School(name=judge_school)
+                            s.save()
+                            schools.append(s.id)
+                        except:
+                            self.errors.append(judge_name + ': Invalid School')
+                            return
+            except IndexError:
+                break
+            col_num += 1
+
+        data = {'name': judge_name, 'rank': judge_rank, 'phone': judge_phone, 'provider': judge_provider, 'schools': schools}
+        form = JudgeForm(data=data)
+        if (form.is_valid()):
+            form.save()
+        else:
+            error_messages = sum([ error[1] for error in form.errors.items() ], [])
+            error_string = ', '.join(error_messages)
+            self.errors.append("%s: %s" % (judge_name, error_string))
+
 
 class TeamImporter(DataImporter):
     num_columns = 9
@@ -52,7 +128,7 @@ class TeamImporter(DataImporter):
                 form.save()
             else:
                 self.errors.append(team_name + ": Invalid School")
-                continue
+                return
             team_school = School.objects.get(name__iexact=school_name)
 
 
@@ -68,16 +144,16 @@ class TeamImporter(DataImporter):
             team_seed = 0
         else:
             self.errors.append(team_name + ': Invalid Seed Value')
-            continue
+            return
 
         deb1_name = self._value_at(row_num, 3)
         if deb1_name == '':
             self.errors.append(team_name + ': Empty Debater-1 Name')
-            continue
+            return
         try:
             Debater.objects.get(name=deb1_name)
             self.errors.append(team_name + ': Duplicate Debater-1 Name')
-            continue
+            return
         except:
             pass
 
@@ -99,9 +175,10 @@ class TeamImporter(DataImporter):
             try:
                 Debater.objects.get(name=deb2_name)
                 self.errors.append(team_name + ': Duplicate Debater-2 Name')
-                continue
+                return
             except:
                 pass
+
             deb2_status = self._value_at(row_num, 8).lower()
             if deb2_status in ['novice', 'nov', 'n']:
                 deb2_status = 1
@@ -125,7 +202,8 @@ class TeamImporter(DataImporter):
             deb1.save()
         except:
             self.errors.append(team_name + ': Unkown Error Saving Debater 1')
-            continue
+            return
+
         if not iron_man:
             try:
                 deb2 = Debater(name=deb2_name, novice_status=deb2_status, phone=deb2_phone, provider=deb2_provider)
@@ -135,18 +213,18 @@ class TeamImporter(DataImporter):
                 self.errors.append(team_name + ': Unkown Error Saving Debater 2')
                 self.errors.append('        WARNING: Debaters on this team may be added to database. ' +
                                     'Please Check this Manually')
-                continue
+                return
 
         team = Team(name=team_name, school=team_school, seed=team_seed)
         try:
             team.save()
             team.debaters.add(deb1)
-            if (not iron_man):
+            if not iron_man:
                 team.debaters.add(deb2)
             else:
                 self.errors.append(team_name + ": Detected to be Iron Man - Still added successfully")
             team.save()
         except:
             self.errors.append(team_name + ': Unknown Error Saving Team')
-            team_errors.append('        WARNING: Debaters on this team may be added to database. ' +
+            self.errors.append('        WARNING: Debaters on this team may be added to database. ' +
                                 'Please Check this Manually')
