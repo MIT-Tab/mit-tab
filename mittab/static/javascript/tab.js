0a9872433e88746a90ab9d4bad8129deadfeb94d
Ben Muschol <benmuschol@gmail.com>
2018-06-04 11:38:16 -0400

    Better multiselect

--- mittab/static/javascript/tab.js
+++ mittab/static/javascript/tab.js
@@ -172,10 +172,42 @@ $(document).ready(function(){
     });
 
     apply_filters();
-    $("[multiple]").multiselect({
-      enableFiltering: true,
-      buttonClass: 'btn btn-outline-secondary',
-      buttonWidth: "20em",
-      enableCaseInsensitiveFiltering: true
-    })
+
+    // copied from http://loudev.com/#usage
+    $('[multiple]').multiSelect({
+        selectableHeader: "<input type='text' class='form-control' autocomplete='off' placeholder='Search'>",
+        selectionHeader: "<input type='text' class='form-control' autocomplete='off' placeholder='Search'>",
+
+        afterInit: function(ms){
+            var that = this,
+                $selectableSearch = that.$selectableUl.prev(),
+                $selectionSearch = that.$selectionUl.prev(),
+                selectableSearchString = '#'+that.$container.attr('id')+' .ms-elem-selectable:not(.ms-selected)',
+                selectionSearchString = '#'+that.$container.attr('id')+' .ms-elem-selection.ms-selected';
+
+            that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+            .on('keydown', function(e){
+                if (e.which === 40){
+                    that.$selectableUl.focus();
+                    return false;
+                }
+            });
+
+            that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+            .on('keydown', function(e){
+                if (e.which == 40){
+                    that.$selectionUl.focus();
+                    return false;
+                }
+            });
+        },
+        afterSelect: function(){
+            this.qs1.cache();
+            this.qs2.cache();
+        },
+        afterDeselect: function(){
+            this.qs1.cache();
+            this.qs2.cache();
+        }
+    });
 });
