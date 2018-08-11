45e1a73ab4cae21b24a9a1020332e79210f70429
Ben Muschol <benmuschol@gmail.com>
2018-08-11 09:51:57 -0700

    Make command more flexible

--- mittab/apps/tab/management/commands/export_stats.py
+++ mittab/apps/tab/management/commands/export_stats.py
@@ -1,3 +1,4 @@
+import os
 import csv
 
 from django.core.management.base import BaseCommand
@@ -11,14 +12,21 @@ class Command(BaseCommand):
     DEBATER_ROWS = ('Name', 'School', 'Speaks', 'Ranks')
 
     help = 'Dump novice & varsity team/speaker rankings as a csv'
+    optionList = BaseCommand.option_list + (
+            make_option("--root", dest="root", help="root path for all of the csv files"),
+            make_option("--team-file", dest="team_file", help="name of the teams file"),
+            make_option("--nov-team-file", dest="nov_team_file", help="name of the novice teams file"),
+            make_option("--debater-file", dest="debater_file", help="name of the debaters file"),
+            make_option("--nov-debater-file", dest="nov_debater_file", help="name of the novice debaters file"),
+            )
 
     def make_team_row(self, team):
         return (
             team.name,
             team.school.name,
-            team.hybrid_school.name if team.hybrid_school else '',
-            team.debaters.first().name if team.debaters.count() else '',
-            team.debaters.last().name if team.debaters.count() > 1 else '',
+            team.hybrid_school.name if team.hybrid_school else "",
+            team.debaters.first().name if team.debaters.count() else "",
+            team.debaters.last().name if team.debaters.count() > 1 else "",
             tab_logic.tot_wins(team),
             tab_logic.tot_speaks(team),
             tab_logic.tot_ranks(team)
@@ -27,7 +35,7 @@ class Command(BaseCommand):
     def make_debater_row(self, debater):
         return (
             debater.name,
-            tab_logic.deb_team(debater).name if tab_logic.deb_team(debater) else '',
+            tab_logic.deb_team(debater).name if tab_logic.deb_team(debater) else "",
             tab_logic.tot_speaks_deb(debater),
             tab_logic.tot_ranks_deb(debater)
         )
@@ -45,9 +53,15 @@ class Command(BaseCommand):
         debaters = [ self.make_debater_row(deb) for deb in tab_logic.rank_speakers() ]
         nov_debaters = [ self.make_debater_row(deb) for deb in tab_logic.rank_nov_speakers() ]
 
+        root = kwargs.get("root", ".")
+        team_file = kwargs.get("team_file", "teams.csv")
+        nov_team_file = kwargs.get("nov_team_file", "nov-teams.csv")
+        debater_file = kwargs.get("debater_file", "debaters.csv")
+        nov_debater_file = kwargs.get("nov_debater_file", "nov-debaters.csv")
+
         print('Writing to csv')
-        self.write_to_csv("teams.csv", self.TEAM_ROWS, teams)
-        self.write_to_csv("nov-teams.csv", self.TEAM_ROWS, nov_teams)
-        self.write_to_csv("debaters.csv", self.DEBATER_ROWS, debaters)
-        self.write_to_csv("nov-debaters.csv", self.DEBATER_ROWS, nov_debaters)
+        self.write_to_csv(os.path.join(root, team_file), self.TEAM_ROWS, teams)
+        self.write_to_csv(os.path.join(root, nov_team_file), self.TEAM_ROWS, nov_teams)
+        self.write_to_csv(os.path.join(root, debater_file), self.DEBATER_ROWS, debaters)
+        self.write_to_csv(os.path.join(root, nov_debater_file), self.DEBATER_ROWS, nov_debaters)
         print('Done!')
