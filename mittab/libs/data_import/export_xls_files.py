d27f5c35166cecbf42de98f12c334607192df4c7
ifly6 <ifly6@me.com>
2019-02-16 21:31:09 -0500

    fixed bug in export xls (can blame python's lack of typing, whoppdydo), wrote the df export commands, included pandas to requirements, updated header names to be consistent with csv general practices, swapped everything over to enumerate instead of java-esque arraylist style

--- mittab/libs/data_import/export_xls_files.py
+++ mittab/libs/data_import/export_xls_files.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
+
+# data stuff
 from xlwt import Workbook
+import pandas as pd
 
 from mittab.apps.tab.models import Team, Judge, Room, Debater
 from mittab.libs import tab_logic
@@ -14,6 +17,43 @@ def _create_vn_str(novice_status):
     return 'NO STATUS'
 
 
+def _seed_to_str(seed_int):
+    if seed_int is 0:
+        return 'unseeded'
+    elif seed_int is 1:
+        return 'free'
+    elif seed_int is 2:
+        return 'half'
+    elif seed_int is 3:
+        return 'full'
+
+    return 'no seed'
+
+
+def export_teams_df():
+    """Exports teams as a new XLS file which is then streamed to an HTTP response. This file should always be
+    cross-compatible with the parsing system used in the import_teams file. """
+
+    entries = []
+    for i, team in enumerate(Team.objects.all()):
+        entry = {
+            'team_name': team.name,
+            'team_school': team.school.name,
+            'team_seed': _seed_to_str(team.seed)
+        }
+
+        for debater_count, debater in enumerate(team.debaters.all()):
+            root = 'debater_{}_'.format(debater_count + 1)
+            entry[root + 'name'] = debater.name
+            entry[root + 'status'] = _create_vn_str(debater.novice_status)
+            entry[root + 'phone'] = debater.phone
+            entry[root + 'provider'] = debater.provider
+
+        entries.append(entry)
+
+    return pd.DataFrame(entries)
+
+
 def export_teams():
     """Exports teams as a new XLS file which is then streamed to an HTTP response. This file should always be
     cross-compatible with the parsing system used in the import_teams file. """
@@ -22,16 +62,14 @@ def export_teams():
     sheet = book.add_sheet('Teams')
 
     # write headers
-    headers = ['Name', 'School', 'Seed', 'Debater 1 Name', 'D1 Status', 'D1 Phone#', 'D1 Provider', 'Debater 2 Name',
-               'D2 Status', 'D2 Phone#', 'D2 Provider']
+    headers = ['team_name', 'team_school', 'team_seed', 'team_debater_1_name', 'team_debater_1_status',
+               'team_debater_1_phone', 'team_debater_1_provider', 'team_debater_2_name',
+               'team_debater_2_status', 'team_debater_2_phone', 'team_debater_2_provider']
     for i in xrange(len(headers)):
         sheet.write(0, i, headers[i])
 
     # write rows
-    teams = Team.objects.all()
-    for i in xrange(len(teams)):
-
-        team = teams[i]
+    for i, team in enumerate(Team.objects.all()):
         row = i + 1
 
         name = team.name
@@ -39,15 +77,7 @@ def export_teams():
 
         # convert seed
         # seed = 0 if unseeded, seed = 1 if free seed, seed = 2 if half seed, seed = 3 if full seed
-        seed = team.seed
-        if seed is 0:
-            seed = 'unseeded'
-        elif seed is 1:
-            seed = 'free'
-        elif seed is 2:
-            seed = 'half'
-        elif seed is 3:
-            seed = 'full'
+        seed = _seed_to_str(team.seed)
 
         debaters = team.debaters.all()
         deb1_name = debaters[0].name
@@ -78,6 +108,25 @@ def export_teams():
     return book
 
 
+def export_judges_df():
+    entries = []
+    for i, judge in enumerate(Judge.objects.all()):
+        entry = {
+            'judge_name': judge.name,
+            'judge_rank': judge.rank,
+            'judge_phone': judge.phone,
+            'judge_provider': judge.provider
+        }
+
+        for judge_i, school in enumerate(judge.schools.all()):
+            entry['judge_school_{}'.format(judge_i + 1)] = school.name
+
+        entries.append(entry)
+
+    df = pd.DataFrame(entries)
+    return df
+
+
 def export_judges():
     """Exports judges to a XLS file which is then streamed to the recipient. This method should always be
     cross-compatible with the import_judges file. """
@@ -85,13 +134,11 @@ def export_judges():
     sheet = book.add_sheet('Judges')
 
     # 0 name, 1 rank, 2 phone, 3 provider, 4+ schools
-    headers = ['Name', 'Rank', 'Phone', 'Provider', 'Schools']
-    for i in xrange(len(headers)):
-        sheet.write(0, i, headers[i])
+    headers = ['judge_name', 'judge_rank', 'judge_phone', 'judge_provider', 'judge_schools']
+    for i, header in enumerate(headers):
+        sheet.write(0, i, header)
 
-    judges = Judge.objects.all()
-    for i in xrange(len(judges)):
-        judge = judges[i]
+    for i, judge in enumerate(Judge.objects.all()):
         row = i + 1
 
         name = judge.name
@@ -105,13 +152,24 @@ def export_judges():
         sheet.write(row, 2, phone)
         sheet.write(row, 3, provider)
 
-        for x in xrange(len(schools)):  # iterate through other school affiliations
-            sheet.write(row, x + 4, schools[x].name)
+        for j, school in enumerate(schools):  # iterate through other school affiliations
+            sheet.write(row, j + 4, school.name)
 
     return book
 
 
-@permission_required('tab.tab_settings.can_change', login_url="/403/")
+def export_rooms_df():
+    entries = []
+    for i, room in enumerate(Room.objects.all()):
+        entry = {
+            'room_name': room.name,
+            'room_rank': room.rank
+        }
+        entries.append(entry)
+
+    return pd.DataFrame(entries)
+
+
 def export_rooms():
     """Exports rooms to an XLS file which is then streamed to the recipient. This method should always be
     cross-compatible with the import_rooms file. """
@@ -119,13 +177,11 @@ def export_rooms():
     sheet = book.add_sheet('Rooms')
 
     # 0 name, 1 rank, 2 phone, 3 provider, 4+ schools
-    headers = ['Name', 'Rank']
-    for i in xrange(len(headers)):
-        sheet.write(0, i, headers[i])
+    headers = ['room_name', 'room_rank']
+    for i, header in enumerate(headers):
+        sheet.write(1, i, header)
 
-    rooms = Room.objects.all()
-    for i in xrange(len(rooms)):
-        room = rooms[i]
+    for i, room in enumerate(Room.objects.all()):
         row = i + 1
         sheet.write(row, 0, room.name)
         sheet.write(row, 1, room.rank)
@@ -133,7 +189,24 @@ def export_rooms():
     return book
 
 
-@permission_required('tab.tab_settings.can_change', login_url="/403/")
+def export_team_stats_df():
+    entries = []
+    for team in Team.objects.all():
+        entries.append({
+            'team_name': team.name,
+            'team_wins': tab_logic.tot_wins(team),
+            'team_speaks': tab_logic.tot_speaks(team),
+            'team_ranks': tab_logic.tot_ranks(team),
+            'team_speaks_single_adj': tab_logic.single_adjusted_speaks(team),
+            'team_ranks_single_adj': tab_logic.single_adjusted_ranks(team),
+            'team_speaks_double_adj': tab_logic.double_adjusted_speaks(team),
+            'team_ranks_double_adj': tab_logic.double_adjusted_ranks(team),
+            'team_opp_str': tab_logic.opp_strength(team)
+        })
+
+    return pd.DataFrame(entries)
+
+
 def export_team_stats():
     """Exports data as XLS for each team: win-loss record, total speaker points, total ranks, single-adjusted
     speaker points, single-adjusted ranks, double-adjusted speaker points, double-adjusted ranks, and opposition
@@ -141,18 +214,18 @@ def export_team_stats():
     book = Workbook('utf-8')
     sheet = book.add_sheet('Team stats')
 
-    sheet.write(0, 0, 'If you are calculating the break off of these statistics, please pay attention to how you sort. '
-                      'In general, everything except ranks should be sorted from large to small.')
+    sheet.write(0, 0,
+                'If you are calculating the break off of these statistics, please pay attention to how you sort. '
+                'In general, everything except ranks should be sorted from large to small.')
 
-    headers = ['team name', '# wins', 'total speaker points', 'total ranks', 'single-adjusted speaker points',
-               'single-adjusted ranks', 'double-adjusted speaker points', 'double-adjusted ranks',
-               'opposition strength']
+    headers = ['team_name', 'team_wins', 'team_speaks', 'team_ranks', 'team_speaks_single_adj',
+               'team_ranks_single_adj', 'team_speaks_double_adj', 'team_ranks_double_adj', 'team_opp_str']
     for i in xrange(len(headers)):
         sheet.write(1, i, headers[i])
 
-    teams = Team.objects.all()
-    for i in xrange(len(teams)):
-        team, row = teams[i], i + 2
+    for i, team in enumerate(Team.objects.all()):
+        row = i + 2
+
         sheet.write(row, 0, team.name)
         sheet.write(row, 1, tab_logic.tot_wins(team))
         sheet.write(row, 2, tab_logic.tot_speaks(team))
@@ -166,7 +239,25 @@ def export_team_stats():
     return book
 
 
-@permission_required('tab.tab_settings.can_change', login_url="/403/")
+def export_debater_stats_df():
+    entries = []
+    for debater in Debater.objects.all():
+        debater_team = Team.objects.get(debaters=debater)
+        entries.append({
+            'debater_name': debater.name,
+            'debater_speaks': tab_logic.tot_speaks_deb(debater, average_ironmen=True),
+            'debater_ranks': tab_logic.tot_ranks_deb(debater, True),
+            'debater_speaks_single_adj': tab_logic.single_adjusted_speaks_deb(debater),
+            'debater_ranks_single_adj': tab_logic.single_adjusted_ranks_deb(debater),
+            'debater_speaks_double_adj': tab_logic.double_adjusted_speaks_deb(debater),
+            'debater_ranks_double_adj': tab_logic.double_adjusted_ranks_deb(debater),
+            'debater_team_wins': tab_logic.tot_wins(debater_team),
+            'debater_team_opp_strength': tab_logic.opp_strength(debater_team)
+        })
+
+    return pd.DataFrame(entries)
+
+
 def export_debater_stats():
     """Exports data as XLS for each speaker: total speaker points, total ranks, single adjusted speaks, single adjusted
     ranks, double adjusted speaks, double adjusted ranks, team performance, opposition strength. Automatically averages
@@ -174,18 +265,20 @@ def export_debater_stats():
     book = Workbook('utf-8')
     sheet = book.add_sheet('Team stats')
 
-    sheet.write(0, 0, 'If you are calculating the awards off these statistics, please pay attention to how you sort. '
-                      'In general, everything except ranks should be sorted from large to small.')
+    sheet.write(0, 0,
+                'If you are calculating the awards off these statistics, please pay attention to how you sort. '
+                'In general, everything except ranks should be sorted from large to small.')
 
-    headers = ['debater name', 'total speaker points', 'total ranks', 'single-adjusted speaker points',
-               'single-adjusted ranks', 'double-adjusted speaker points', 'double-adjusted ranks',
-               'team performance (debater\'s team win#)', 'opposition strength']
-    for i in xrange(len(headers)):
-        sheet.write(1, i, headers[i])
+    headers = ['debater_name', 'debater_speaks', 'debater_ranks', 'debater_speaks_single_adj',
+               'debater_ranks_single_adj', 'debater_speaks_double_adj', 'debater_ranks_double_adj', 'debater_team_wins',
+               'debater_team_opp_strength']
+    for i, header in enumerate(headers):
+        sheet.write(1, i, header)
 
-    debaters = Debater.objects.all()
-    for i in xrange(len(debaters)):
-        debater, row = debaters[i], i + 2
+    for i, debater in enumerate(Debater.objects.all()):
+        row = i + 2
+        debater_team = Team.objects.get(debaters=debater)
+
         sheet.write(row, 0, debater.name)
         sheet.write(row, 1, tab_logic.tot_speaks_deb(debater, average_ironmen=True))
         sheet.write(row, 2, tab_logic.tot_ranks_deb(debater, True))
@@ -193,7 +286,9 @@ def export_debater_stats():
         sheet.write(row, 4, tab_logic.single_adjusted_ranks_deb(debater))
         sheet.write(row, 5, tab_logic.double_adjusted_speaks_deb(debater))
         sheet.write(row, 6, tab_logic.double_adjusted_ranks_deb(debater))
-        sheet.write(row, 7, tab_logic.tot_wins(Team.objects.get(debaters__name=debater.name)))
-        sheet.write(row, 8, tab_logic.opp_strength(debater))
+
+        # debater stats
+        sheet.write(row, 7, tab_logic.tot_wins(debater_team))
+        sheet.write(row, 8, tab_logic.opp_strength(debater_team))
 
     return book
