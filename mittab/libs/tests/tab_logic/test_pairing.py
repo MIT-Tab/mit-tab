d60d4b2a7fe641548c383685b3243b7cd305df82
Ben Muschol <benmuschol@gmail.com>
2019-05-05 16:42:01 -0400

    try parallel

--- mittab/libs/tests/tab_logic/test_pairing.py
+++ mittab/libs/tests/tab_logic/test_pairing.py
@@ -1,59 +1,11 @@
-from django.test import TestCase
-from mittab.apps.tab.models import CheckIn
-from mittab.apps.tab.models import Debater
-from mittab.apps.tab.models import Judge
-from mittab.apps.tab.models import Team
-from mittab.apps.tab.models import TabSettings
-from mittab.apps.tab.models import Round
-import mittab.libs.assign_judges as assign_judges
-import mittab.libs.tab_logic as tab_logic
-from mittab.libs.tests.assertion import assert_nearly_equal
-from mittab.libs.tests.data.load_data import load_debater_rankings
-from mittab.libs.tests.data.load_data import load_team_rankings
-from mittab.libs.tests.helpers import generate_results
-from mittab.libs.tab_logic.rankings import TeamScore, DebaterScore
-
 import random
 
+from django.test import TestCase
 
-class TestRankingLogic(TestCase):
-    """Tests that the methods related to debater and team scoring work as
-    expected"""
-    fixtures = ['testing_finished_db']
-
-    def test_debater_score(self):
-        """ Comprehensive test of ranking calculations, done on real world
-        data that has real world problems (e.g. teams not paired in, ironmen,
-        etc ...)
-        """
-        debaters = Debater.objects.all()
-        scores = [(debater.name, DebaterScore(debater).scoring_tuple())
-                  for debater in debaters]
-        expected_scores = load_debater_rankings()
-        dict_scores, dict_expected_scores = list(map(dict,
-                                                (scores, expected_scores)))
-
-        assert len(dict_scores) == len(dict_expected_scores)
-        for k in dict_scores:
-            left, right = dict_scores[k], dict_expected_scores[k]
-            msg = "{} - {}, {}".format(k, left, right)
-            [assert_nearly_equal(*pair, message=msg) for pair
-             in zip(set(left), set(right))]
-
-    def test_team_score(self):
-        """ Comprehensive test of team scoring calculations, done on real
-        world data that has real world inaccuracies """
-        teams = Team.objects.all()
-        scores = [(team.name, TeamScore(team).scoring_tuple()) for team in teams]
-        expected_scores = load_team_rankings()
-        dict_scores, dict_expected_scores = list(map(dict,
-                                                (scores, expected_scores)))
-        assert len(dict_scores) == len(dict_expected_scores)
-        for k in dict_scores:
-            left, right = dict_scores[k], dict_expected_scores[k]
-            msg = "{} - {}, {}".format(k, left, right)
-            [assert_nearly_equal(*pair, message=msg) for pair
-             in zip(set(left), set(right))]
+from mittab.apps.tab.models import CheckIn, Judge, TabSettings, Round
+from mittab.libs import assign_judges
+from mittab.libs import tab_logic
+from mittab.libs.tests.helpers import generate_results
 
 class TestPairingLogic(TestCase):
     """
@@ -119,5 +71,3 @@ class TestPairingLogic(TestCase):
         last_round = 6
         for _ in range(1, last_round):
             self.check_pairing(self.round_number(), last_round)
-
-
---
+++ mittab/libs/tests/test_case.py
@@ -0,0 +1,70 @@
+import time
+
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from splinter import Browser
+
+class BaseWebTestCase(LiveServerTestCase):
+    """
+    Abstract class to handle logic for web tests
+    """
+    username = 'tab'
+    password = 'password'
+    wait_seconds = 3.0
+
+    def setUp(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--no-sandbox")
+        self.browser = Browser('chrome', headless=True, wait_time=10, options=chrome_options)
+        super(BaseWebTestCase, self).setUp()
+
+    def tearDown(self):
+        self.browser.quit()
+        super(BaseWebTestCase, self).tearDown()
+
+    def _post_teardown(self):
+        print("executing")
+        try:
+            super(BaseWebTestCase, self)._post_teardown()
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            print("Ignoring exception in post-teardown")
+
+    def _wait(self):
+        time.sleep(self.wait_seconds)
+
+    def _login(self):
+        self._visit("")
+        self.browser.fill('username', self.username)
+        self.browser.fill('password', self.password)
+        self.browser.find_by_text('Sign in').first.click()
+
+        assert self.browser.is_text_present('Home')
+        assert not self.browser.is_text_present('Sign in')
+
+    def _go_home(self):
+        self.browser.click_link_by_text('Home')
+        self._wait()
+
+    def _setup_confirm(self):
+        """
+        First part of work-around to let phantomjs accept confirmation dialogs
+        http://stackoverflow.com/questions/19903146/confirm-alert-window-in-phantom-js
+        """
+        js_confirm = 'window.confirm = function() { return true }'
+        self.browser.execute_script(js_confirm)
+
+    def _accept_confirm(self):
+        """
+        Second part of work-around to let phantomjs accept confirmation dialogs
+        MUST call self._setup_confirm() for this to work
+        """
+        self.browser.execute_script('return window.confirm')
+
+    def _visit(self, path):
+        path = self.live_server_url + path
+        self.browser.visit(path)
+        self._wait()
