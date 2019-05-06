d75cf40a34e4695af6dcf30eafa220f7812229c4
Ben Muschol <benmuschol@gmail.com>
2019-05-06 00:57:36 -0400

    Get webpack vaguely working

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,29 @@ module.exports = {
   context: __dirname,
   entry: './assets/js/index',
   output: {
-      path: path.resolve('./assets/webpack_bundles/'),
-      filename: "[name]-[hash].js"
+    path: path.resolve('./assets/webpack_bundles/'),
+    filename: "[name]-[hash].js"
   },
+  module: {
+    rules: [{
+      test: /\.css/,
+      use: [
+        "style-loader",
+        "css-loader"
+      ]
+    }, {
+      test: /\.scss$/,
+      use: [
+        "style-loader", // creates style nodes from JS strings
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+      ]
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.scss', '.css'],
+  },
+
 
   plugins: [
     new BundleTracker({filename: './webpack-stats.json'})
