0858c21b8b5ef169831ea551f4ebc4399c86830b
Ben Muschol <benmuschol@gmail.com>
2019-05-12 13:22:42 -0400

    Select2 for selects + bootstrap4 theme

--- assets/js/multiselect.js
+++ assets/js/multiselect.js
@@ -1,9 +1,13 @@
 import $ from 'jquery'
+import select2 from 'select2'
+import 'select2/dist/css/select2.css'
+import '@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css'
 
-import 'multiselect'
-
-function multiselectInit() {
-  $('[multiple]').multiSelect()
+function multiselectInit($elem) {
+  if (!$elem) {
+    $elem = $('#data-entry-form select[multiple]')
+  }
+  $elem.select2({ theme: 'bootstrap4' })
 }
 
 export default multiselectInit
