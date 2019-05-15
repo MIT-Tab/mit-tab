dcf96ff0f1b0984b7843dc83bdcdc86c4cd3d247
Ben Muschol <benmuschol@gmail.com>
2019-05-15 12:34:58 -0400

    Forgot a commit

--- mittab/libs/tests/data/load_data.py
+++ mittab/libs/tests/data/load_data.py
@@ -1,14 +1,14 @@
 import json
 import os
 
-base_dir = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(__file__)
 
 
 def load_debater_rankings():
     return json.load(
-        open(os.path.join(base_dir, "debater_finished_scores.json"), "r"))
+        open(os.path.join(BASE_DIR, "debater_finished_scores.json"), "r"))
 
 
 def load_team_rankings():
     return json.load(
-        open(os.path.join(base_dir, "team_finished_scores.json"), "r"))
+        open(os.path.join(BASE_DIR, "team_finished_scores.json"), "r"))
