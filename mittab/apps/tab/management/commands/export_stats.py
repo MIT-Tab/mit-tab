4bdbc2664c6bb4610157758064c35c259f61463d
Ben Muschol <benmuschol@gmail.com>
2019-05-18 11:49:06 -0400

    Run the formatter

--- mittab/apps/tab/management/commands/export_stats.py
+++ mittab/apps/tab/management/commands/export_stats.py
@@ -9,42 +9,47 @@ from mittab.libs import tab_logic
 
 
 class Command(BaseCommand):
-    TEAM_ROWS = ('Team Name', 'School', 'Hyrbid School', 'Debater 1', 'Debater 2', 'Wins', 'Speaks', 'Ranks')
+    TEAM_ROWS = ('Team Name', 'School', 'Hyrbid School', 'Debater 1',
+                 'Debater 2', 'Wins', 'Speaks', 'Ranks')
     DEBATER_ROWS = ('Name', 'School', 'Speaks', 'Ranks')
 
     help = 'Dump novice & varsity team/speaker rankings as a csv'
 
     def add_arguments(self, parser):
-        parser.add_argument("--root", dest="root",
-            default=".", help="root path for all of the csv files"),
-        parser.add_argument("--team-file", dest="team_file",
-            default="teams.csv", help="name of the teams file"),
-        parser.add_argument("--nov-team-file", dest="nov_team_file",
-            default="nov-teams.csv", help="name of the novice teams file"),
-        parser.add_argument("--debater-file", dest="debater_file",
-            default="debaters.csv", help="name of the debaters file"),
-        parser.add_argument("--nov-debater-file", dest="nov_debater_file",
-            default="nov-debaters.csv", help="name of the novice debaters file")
+        parser.add_argument("--root",
+                            dest="root",
+                            default=".",
+                            help="root path for all of the csv files"),
+        parser.add_argument("--team-file",
+                            dest="team_file",
+                            default="teams.csv",
+                            help="name of the teams file"),
+        parser.add_argument("--nov-team-file",
+                            dest="nov_team_file",
+                            default="nov-teams.csv",
+                            help="name of the novice teams file"),
+        parser.add_argument("--debater-file",
+                            dest="debater_file",
+                            default="debaters.csv",
+                            help="name of the debaters file"),
+        parser.add_argument("--nov-debater-file",
+                            dest="nov_debater_file",
+                            default="nov-debaters.csv",
+                            help="name of the novice debaters file")
 
     def make_team_row(self, team):
-        return (
-            team.name,
-            team.school.name,
-            team.hybrid_school.name if team.hybrid_school else "",
-            team.debaters.first().name if team.debaters.count() else "",
-            team.debaters.last().name if team.debaters.count() > 1 else "",
-            tab_logic.tot_wins(team),
-            tab_logic.tot_speaks(team),
-            tab_logic.tot_ranks(team)
-        )
+        return (team.name, team.school.name,
+                team.hybrid_school.name if team.hybrid_school else "",
+                team.debaters.first().name if team.debaters.count() else "",
+                team.debaters.last().name if team.debaters.count() > 1 else "",
+                tab_logic.tot_wins(team), tab_logic.tot_speaks(team),
+                tab_logic.tot_ranks(team))
 
     def make_debater_row(self, debater):
-        return (
-            debater.name,
-            tab_logic.deb_team(debater).name if tab_logic.deb_team(debater) else "",
-            tab_logic.tot_speaks_deb(debater),
-            tab_logic.tot_ranks_deb(debater)
-        )
+        return (debater.name, tab_logic.deb_team(debater).name
+                if tab_logic.deb_team(debater) else "",
+                tab_logic.tot_speaks_deb(debater),
+                tab_logic.tot_ranks_deb(debater))
 
     def write_to_csv(self, filename, headers, rows):
         with open(filename, "w") as f:
@@ -56,18 +61,27 @@ class Command(BaseCommand):
         if not os.path.exists(kwargs["root"]): os.makedirs(kwargs["root"])
 
         print('Calculating ranks')
-        teams = [ self.make_team_row(team) for team in tab_logic.rank_teams() ]
-        nov_teams = [ self.make_team_row(team) for team in tab_logic.rank_nov_teams() ]
-        debaters = [ self.make_debater_row(deb) for deb in tab_logic.rank_speakers() ]
-        nov_debaters = [ self.make_debater_row(deb) for deb in tab_logic.rank_nov_speakers() ]
+        teams = [self.make_team_row(team) for team in tab_logic.rank_teams()]
+        nov_teams = [
+            self.make_team_row(team) for team in tab_logic.rank_nov_teams()
+        ]
+        debaters = [
+            self.make_debater_row(deb) for deb in tab_logic.rank_speakers()
+        ]
+        nov_debaters = [
+            self.make_debater_row(deb)
+            for deb in tab_logic.rank_nov_speakers()
+        ]
 
         print('Writing to csv')
         self.write_to_csv(os.path.join(kwargs["root"], kwargs["team_file"]),
-                self.TEAM_ROWS, teams)
-        self.write_to_csv(os.path.join(kwargs["root"], kwargs["nov_team_file"]),
-                self.TEAM_ROWS, nov_teams)
+                          self.TEAM_ROWS, teams)
+        self.write_to_csv(
+            os.path.join(kwargs["root"], kwargs["nov_team_file"]),
+            self.TEAM_ROWS, nov_teams)
         self.write_to_csv(os.path.join(kwargs["root"], kwargs["debater_file"]),
-                self.DEBATER_ROWS, debaters)
-        self.write_to_csv(os.path.join(kwargs["root"], kwargs["nov_debater_file"]),
-                self.DEBATER_ROWS, nov_debaters)
+                          self.DEBATER_ROWS, debaters)
+        self.write_to_csv(
+            os.path.join(kwargs["root"], kwargs["nov_debater_file"]),
+            self.DEBATER_ROWS, nov_debaters)
         print('Done!')
--- mittab/apps/tab/management/commands/initialize_tourney.py
+++ mittab/apps/tab/management/commands/initialize_tourney.py
@@ -11,29 +11,31 @@ from django.core.management.base import BaseCommand, CommandError
 from mittab.libs.backup import BACKUP_PREFIX
 from mittab.apps.tab.models import TabSettings
 
+
 class Command(BaseCommand):
     help = 'Setup a new tounament and backup the last one'
 
-
-
     def add_arguments(self, parser):
-        parser.add_argument('--tab-password',
-                dest='tab_password',
-                help='Password for the tab user',
-                nargs='?',
-                default=User.objects.make_random_password(length=8))
-        parser.add_argument('--entry-password',
-                dest='entry_password',
-                help='Password for the entry user',
-                nargs='?',
-                default=User.objects.make_random_password(length=8))
+        parser.add_argument(
+            '--tab-password',
+            dest='tab_password',
+            help='Password for the tab user',
+            nargs='?',
+            default=User.objects.make_random_password(length=8))
+        parser.add_argument(
+            '--entry-password',
+            dest='entry_password',
+            help='Password for the entry user',
+            nargs='?',
+            default=User.objects.make_random_password(length=8))
         parser.add_argument('backup_directory')
 
     def handle(self, *args, **options):
         backup_dir = options['backup_directory']
         path = BACKUP_PREFIX
 
-        self.stdout.write("Creating directory for current tournament in backup directory")
+        self.stdout.write(
+            "Creating directory for current tournament in backup directory")
         tournament_dir = os.path.join(backup_dir, str(int(time.time())))
 
         if not os.path.exists(tournament_dir):
@@ -42,15 +44,17 @@ class Command(BaseCommand):
         if not os.path.exists(path + "/backups"):
             os.makedirs(path + "/backups")
 
-        self.stdout.write("Copying current tournament state to backup tournament directory: %s" % tournament_dir)
+        self.stdout.write(
+            "Copying current tournament state to backup tournament directory: %s"
+            % tournament_dir)
         try:
             shutil.copy(path + "/pairing_db.sqlite3", tournament_dir)
             shutil.rmtree(tournament_dir + "/backups", ignore_errors=True)
             shutil.copytree(path + "/backups", tournament_dir + "/backups")
         except (IOError, os.error) as why:
-           self.stdout.write("Failed to backup current tournament state")
-           print(why)
-           sys.exit(1)
+            self.stdout.write("Failed to backup current tournament state")
+            print(why)
+            sys.exit(1)
 
         self.stdout.write("Clearing data from database")
         try:
@@ -66,7 +70,8 @@ class Command(BaseCommand):
         tab.is_admin = True
         tab.is_superuser = True
         tab.save()
-        entry = User.objects.create_user("entry", None, options["entry_password"])
+        entry = User.objects.create_user("entry", None,
+                                         options["entry_password"])
 
         self.stdout.write("Setting default tab settings")
         TabSettings.set("tot_rounds", 5)
@@ -78,12 +83,19 @@ class Command(BaseCommand):
             shutil.rmtree(path + "/backups")
             os.makedirs(path + "/backups")
         except (IOError, os.error) as why:
-            self.stdout.write("Failed to copy clean database to pairing_db.sqlite3")
+            self.stdout.write(
+                "Failed to copy clean database to pairing_db.sqlite3")
             print(why)
             sys.exit(1)
 
-        self.stdout.write("Done setting up tournament, after backing up old one. New tournament information:")
-        self.stdout.write("%s | %s" % ("Username".ljust(10," "), "Password".ljust(10, " ")))
-        self.stdout.write("%s | %s" % ("tab".ljust(10," "), options['tab_password'].ljust(10, " ")))
-        self.stdout.write("%s | %s" % ("entry".ljust(10," "), options['entry_password'].ljust(10, " ")))
-
+        self.stdout.write(
+            "Done setting up tournament, after backing up old one. New tournament information:"
+        )
+        self.stdout.write(
+            "%s | %s" % ("Username".ljust(10, " "), "Password".ljust(10, " ")))
+        self.stdout.write(
+            "%s | %s" %
+            ("tab".ljust(10, " "), options['tab_password'].ljust(10, " ")))
+        self.stdout.write(
+            "%s | %s" %
+            ("entry".ljust(10, " "), options['entry_password'].ljust(10, " ")))
