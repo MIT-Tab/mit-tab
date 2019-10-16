04dcbd535ce65a03e66970f1f865658df8bed15d
Rodda John <rj2485@columbia.edu>
2019-10-16 10:40:26 -0400

    Added outround unit tests

---
+++ mittab/libs/tests/tab_logic/test_outround_pairing.py
@@ -0,0 +1,28 @@
+import random
+
+from django.test import TestCase
+import pytest
+
+from mittab.apps.tab.models import *
+from mittab.libs import outround_tab_logic
+
+
+@pytest.mark.django_db
+class TestOutroundPairingLogic(TestCase):
+    fixtures = ["testing_finished_db"]
+    pytestmark = pytest.mark.django_db
+
+    def test_break(self):
+        outround_tab_logic.perform_the_break()
+
+    def generate_checkins(self):
+        for r in Room.objects.all():
+            RoomCheckIn.objects.create(room=r,
+                                       round_number=0)
+
+    def test_pairing(self, round_number, last):
+        self.generate_checkins()
+
+        outround_tab_logic.perform_the_break()
+        outround_tab_logic.pair(BreakingTeam.NOVICE)
+        outround_tab_logic.pair(BreakingTeam.VARSITY)
