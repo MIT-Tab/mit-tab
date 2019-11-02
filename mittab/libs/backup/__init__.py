4a79f16f6aa3222790a18fed4727f75a8c806147
Ben Muschol <ben.muschol@airbnb.com>
2019-11-02 16:53:07 -0700

    Some more progress

---
+++ mittab/libs/backup/__init__.py
@@ -0,0 +1,53 @@
+import shutil
+import time
+import os
+from wsgiref.util import FileWrapper
+
+from django.conf import settings
+
+from mittab.apps.tab.models import TabSettings
+from mittab.libs import errors
+from mittab.settings import BASE_DIR
+from mittab.libs.backup.strategies.local_dump import LocalDump
+
+
+def backup_round(dst_filename=None, round_number=None, btime=None):
+    if round_number is None:
+        round_number = TabSettings.get("cur_round")
+
+    if btime is None:
+        btime = int(time.time())
+
+    print("Trying to backup to backups directory")
+    if dst_filename is None:
+        dst_filename = "site_round_%i_%i" % (round_number, btime)
+
+    if LocalDump(dst_filename).exists():
+        dst_filename += "_%i" % btime
+
+    return LocalDump(dst_filename).backup()
+
+
+def handle_backup(f):
+    dst_filename = get_backup_filename(f.name)
+    print(("Tried to write {}".format(dst_filename)))
+    try:
+        with open(dst_filename, "wb+") as destination:
+            for chunk in f.chunks():
+                destination.write(chunk)
+    except Exception:
+        errors.emit_current_exception()
+
+
+def list_backups():
+    print("Checking backups directory")
+    return [ dump.key for dump in LocalDump.all() ]
+
+
+def restore_from_backup(src_key):
+    print("Restoring from backups directory")
+    return LocalDump(src_key).restore()
+
+
+def get_wrapped_file(src_key):
+    return LocalDump(src_key).downloadable()
---
+++ mittab/libs/backup/strategies/local_dump.py
@@ -0,0 +1,45 @@
+import os
+from io import StringIO
+from shutil import copyfileobj
+
+from django.core.management import call_command
+
+from mittab.settings import BASE_DIR
+
+
+BACKUP_PREFIX = os.path.join(BASE_DIR, "mittab")
+BACKUP_PATH = os.path.join(BACKUP_PREFIX, "backups")
+SUFFIX = ".dump.json"
+
+if not os.path.exists(BACKUP_PATH):
+    os.makedirs(BACKUP_PATH)
+
+class LocalDump:
+    def __init__(self, key):
+        self.key = key
+
+    @classmethod
+    def all(cls):
+        all_names = os.listdir(BACKUP_PATH)
+        def key_from_filename(name):
+            return name[:-len(SUFFIX)]
+        return [ cls(key_from_filename(name)) for name in all_names ]
+
+    def backup(self):
+        out = StringIO()
+        call_command("dumpdata", stdout=out)
+        with open(self._get_backup_filename(), 'w') as f:
+            out.seek(0)
+            copyfileobj(out, f)
+
+    def restore(self):
+        return call_command("loaddata", self._get_backup_filename())
+
+    def exists(self):
+        return os.path.exists(self._get_backup_filename())
+
+    def _get_backup_filename(self):
+        key = self.key
+        if len(key) < len(SUFFIX) or not key.endswith(SUFFIX):
+            key += ".dump.json"
+        return os.path.join(BACKUP_PATH, key)
