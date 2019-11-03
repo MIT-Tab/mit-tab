42f7f05bdba11f68045f56939712eee937657346
Ben Muschol <ben.muschol@airbnb.com>
2019-11-02 18:47:11 -0700

    Get it working with docker-compose and mysql

--- mittab/settings.py
+++ mittab/settings.py
@@ -41,10 +41,10 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "mittab",
-        "USER": os.environ.get("MITTAB_DB_USER", "root"),
-        "PASSWORD": os.environ.get("MITTAB_DB_PASS", ""),
-        "HOST": os.environ.get("MITTAB_DB_HOST", "localhost"),
-        "PORT": os.environ.get("MITTAB_DB_PORT", "3306"),
+        "USER": os.environ.get("MYSQL_USER", "root"),
+        "PASSWORD": os.environ.get("MYSQL_PASSWORD", ""),
+        "HOST": os.environ.get("MITTAB_DB_HOST", "127.0.0.1"),
+        "PORT": os.environ.get("MYSQL_PORT", "3306"),
         "ATOMIC_REQUESTS": True,
     }
 }
