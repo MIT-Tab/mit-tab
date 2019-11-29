5af2f35195e397055fd5fa6a7f3aab11c305aa33
Ben Muschol <benmuschol@gmail.com>
2019-11-29 13:08:51 -0500

    Redirect traffic to a page w/o db access during backup write/restore (#285)

--- mittab/libs/backup/__init__.py
+++ mittab/libs/backup/__init__.py
@@ -11,6 +11,18 @@ from mittab.settings import BASE_DIR
 from mittab.libs.backup.strategies.local_dump import LocalDump
 
 
+ACTIVE_BACKUP_KEY = "MITTAB_ACTIVE_BACKUP"
+ACTIVE_BACKUP_VAL = "1"
+
+
+class ActiveBackupContextManager:
+    def __enter__(self):
+        os.environ[ACTIVE_BACKUP_KEY] = ACTIVE_BACKUP_VAL
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.environ[ACTIVE_BACKUP_KEY] = "0"
+
 def _generate_unique_key(base):
     if LocalDump(base).exists():
         return "%s_%s" % (base, int(time.time()))
@@ -18,18 +30,19 @@ def _generate_unique_key(base):
         return base
 
 def backup_round(dst_filename=None, round_number=None, btime=None):
-    if round_number is None:
-        round_number = TabSettings.get("cur_round", "no-round-number")
+    with ActiveBackupContextManager() as _:
+        if round_number is None:
+            round_number = TabSettings.get("cur_round", "no-round-number")
 
-    if btime is None:
-        btime = int(time.time())
+        if btime is None:
+            btime = int(time.time())
 
-    print("Trying to backup to backups directory")
-    if dst_filename is None:
-        dst_filename = "site_round_%i_%i" % (round_number, btime)
+        print("Trying to backup to backups directory")
+        if dst_filename is None:
+            dst_filename = "site_round_%i_%i" % (round_number, btime)
 
-    dst_filename = _generate_unique_key(dst_filename)
-    return LocalDump(dst_filename).backup()
+        dst_filename = _generate_unique_key(dst_filename)
+        return LocalDump(dst_filename).backup()
 
 
 def handle_backup(f):
@@ -47,9 +60,13 @@ def list_backups():
 
 
 def restore_from_backup(src_key):
-    print("Restoring from backups directory")
-    return LocalDump(src_key).restore()
+    with ActiveBackupContextManager() as _:
+        print("Restoring from backups directory")
+        return LocalDump(src_key).restore()
 
 
 def get_wrapped_file(src_key):
     return LocalDump(src_key).downloadable()
+
+def is_backup_active():
+    return str(os.environ.get(ACTIVE_BACKUP_KEY, "0")) == ACTIVE_BACKUP_VAL
