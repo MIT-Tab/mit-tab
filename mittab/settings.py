ced93b8b2f169fe5b897158f2ee49b3ce6dacdc5
Ben Muschol <ben.muschol@airbnb.com>
2020-05-17 12:57:11 -0700

    Initial tasks implementation

--- mittab/settings.py
+++ mittab/settings.py
@@ -18,7 +18,8 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = ("django.contrib.admin", "django.contrib.auth",
                   "django.contrib.contenttypes", "django.contrib.sessions",
                   "django.contrib.messages", "django.contrib.staticfiles",
-                  "mittab.apps.tab", "raven.contrib.django.raven_compat",
+                  "mittab.apps.tasks", "mittab.apps.tab",
+                  "raven.contrib.django.raven_compat",
                   "webpack_loader", "bootstrap4", "polymorphic")
 
 MIDDLEWARE = (
