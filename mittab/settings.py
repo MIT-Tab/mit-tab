b0342d104daf0c7ea6bdbcd406a409fb1b1849cc
Rodda John <johnrod.john@gmail.com>
2020-09-10 13:35:10 -0400

    ok trying again

--- mittab/settings.py
+++ mittab/settings.py
@@ -37,6 +37,13 @@ ROOT_URLCONF = "mittab.urls"
 
 WSGI_APPLICATION = "mittab.wsgi.application"
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#    }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
