6dbaad81d87645c56e187e5385904aee4222c5ea
Rodda John <johnrod.john@gmail.com>
2020-09-10 09:51:07 -0400

    added discord to pipenv

--- start_discord.py
+++ start_discord.py
@@ -2,10 +2,10 @@ import discord, os, sys
 from mit_tab_bot import MITTabClient
 
 kwargs = {}
-guild_id = sys.argv[1] if len(sys.argv) > 1 else None
+tournament_name = sys.argv[1] if len(sys.argv) > 1 else None
 
-if guild_id:
-    kwargs['guild_id'] = guild_id
+if tournament_name:
+    kwargs['tournament_name'] = tournament_name
 
 client = MITTabClient(**kwargs)
 client.run(os.environ.get('DISCORD_BOT_TOKEN'))
