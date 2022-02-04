01c2fee62976532e35a76d279e04a3f94bf5cdd8
Rodda John <johnrod.john@gmail.com>
2022-02-04 14:01:44 -0500

    quick change

--- start_discord.py
+++ start_discord.py
@@ -1,11 +1,5 @@
 import discord, os, sys
 from mit_tab_bot import MITTabClient
 
-kwargs = {}
-guild_id = sys.argv[1] if len(sys.argv) > 1 else None
-
-if tournament_name:
-    kwargs['guild_id'] = guild_id
-
-client = MITTabClient(**kwargs)
+client = MITTabClient(guild_id=sys.argv[1])
 client.run(os.environ.get('DISCORD_BOT_TOKEN'))
