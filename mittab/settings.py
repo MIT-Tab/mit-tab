ca3b41e1387397cc76012e60f0e2c5eb54120a34
Ben Muschol <ben.muschol@airbnb.com>
2022-02-10 22:36:44 -0500

    Fix polymorphic

--- mittab/settings.py
+++ mittab/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = ("django.contrib.admin", "django.contrib.auth",
                   "django.contrib.contenttypes", "django.contrib.sessions",
                   "django.contrib.messages", "django.contrib.staticfiles",
                   "mittab.apps.tab", "raven.contrib.django.raven_compat",
-                  "webpack_loader", "bootstrap4", "polymorphic", "rest_framework",
+                  "webpack_loader", "bootstrap4", "rest_framework",
                   "django_filters")
 
 MIDDLEWARE = (
