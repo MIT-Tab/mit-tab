6da34ff8eddb9df87a7209b17977a3d646f0b59c
JoeyRubas <joeymrubas@gmail.com>
2025-10-03 09:16:52 -0400

    show judges by rank

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -23,8 +23,26 @@ function applyFilters() {
   filterOnFlags(filterGroups);
 }
 
+function toggleJudgeDisplay() {
+  const button = $("#toggle-judge-display");
+  const details = $(".item-detail");
+  
+  if (button.text() === "Show Rank") {
+    details.each((index, element) => {
+      $(element).text($(element).data("rank"));
+    });
+    button.text("Show Code");
+  } else {
+    details.each((index, element) => {
+      $(element).text($(element).data("code"));
+    });
+    button.text("Show Rank");
+  }
+}
+
 function filtersInit() {
   $(".filter").change(applyFilters);
+  $("#toggle-judge-display").click(toggleJudgeDisplay);
   applyFilters();
 }
 
