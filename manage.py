17d7d9b265e53eee6c55874cfbad0170004fd7e5
JoeyRubas <joeymrubas@gmail.com>
2026-05-08 13:29:20 -0400

    Load local env for development commands

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
 if __name__ == "__main__":
+    load_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mittab.settings")
 
     from django.core.management import execute_from_command_line
